8a28471feb2ea64247369c1de0ec4204fc9eed5f
Simon <simondud@hotmail.com>
2018-05-28 09:22:13 +0300

    Fix .shortpixel files not closed on some cases

--- lib/ShortPixel/persist/TextPersister.php
+++ lib/ShortPixel/persist/TextPersister.php
@@ -173,8 +173,14 @@ class TextPersister implements Persister {
                 }
 
             }
+            if($toClose) {
+                $this->closeMetaFile($persistFolder);
+            }
         }
         catch(ClientException $e) {
+            if($toClose) {
+                $this->closeMetaFile($persistFolder);
+            }
             $info = (object)array('status' => 'error', 'message' => $e->getMessage(), 'code' => $e->getCode());
         }
         catch(\Exception $e) { //that should've been a finally but we need to be PHP5.4 compatible...
