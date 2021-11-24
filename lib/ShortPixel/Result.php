3ce73d6d5c11031579bb036d7059b570b00e2255
Simon <simondud@hotmail.com>
2021-11-24 19:05:21 +0200

    Fix #27 Warning: preg_match(): Compilation failed: missing closing parenthesis at offset 16 on Windows 10

--- lib/ShortPixel/Result.php
+++ lib/ShortPixel/Result.php
@@ -43,7 +43,7 @@ class Result {
 
         if($path) {
             $path = rtrim($path, '/\\');
-            if(   (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' && preg_match('/^[a-zA-Z]:(\/|\\)/', $path) === 0) //it's Windows and no drive letter X:
+            if(   (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' && preg_match('/^[a-zA-Z]:(\/|\\\)/', $path) === 0) //it's Windows and no drive letter X:
                || (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN' && substr($path, 0, 1) !== '/')) { //it's not Windows and doesn't start with a /
                 $path = SPTools::trailingslashit(ShortPixel::opt("base_path") ?: $thisDir) . $path;
             }
